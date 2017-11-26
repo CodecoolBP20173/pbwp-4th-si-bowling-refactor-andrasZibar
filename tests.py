ad851ecfecc1925d4f46a96f81c2450a9ad4fccc
Andras Zibar <zibandris@gmail.com>
2017-11-26 22:48:33 +0100

    autmatic changes due to function names changed

--- tests.py
+++ tests.py
@@ -1,24 +1,24 @@
 import unittest
-from bowling import score
+from bowling import getScoreFor
 
 class MyTests(unittest.TestCase):
     def test_gutter_game(self):
-        self.assertEqual(score("--------------------"),  0)
+        self.assertEqual(getScoreFor("--------------------"),  0)
     def test_all_ones(self):
-        self.assertEqual(score("11111111111111111111"),  20)
+        self.assertEqual(getScoreFor("11111111111111111111"),  20)
     def test_one_spare(self):
-        self.assertEqual(score("5/3-----------------"),  16)
+        self.assertEqual(getScoreFor("5/3-----------------"),  16)
     def test_roll_strike(self):
-        self.assertEqual(score("X34----------------"),   24)
+        self.assertEqual(getScoreFor("X34----------------"),   24)
     def test_perfect_game(self):
-        self.assertEqual(score("XXXXXXXXXXXX"),          300)
+        self.assertEqual(getScoreFor("XXXXXXXXXXXX"),          300)
     def test_case_sensitivity(self):
-        self.assertEqual(score("XXxxxXXXXxXx"),          300)
+        self.assertEqual(getScoreFor("XXxxxXXXXxXx"),          300)
     def test_everything_is_fine_please(self):
-        self.assertEqual(score("11111111112222222222"), 30)
-        self.assertEqual(score("5/11------------3/11"), 26)
-        self.assertEqual(score("1/35XXX458/X3/23"),     160)
-        self.assertEqual(score("1/35XXX458/X3/XX6"),    189)
+        self.assertEqual(getScoreFor("11111111112222222222"), 30)
+        self.assertEqual(getScoreFor("5/11------------3/11"), 26)
+        self.assertEqual(getScoreFor("1/35XXX458/X3/23"),     160)
+        self.assertEqual(getScoreFor("1/35XXX458/X3/XX6"),    189)
 
 if __name__ == '__main__':
     unittest.main()
